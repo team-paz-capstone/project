2997e25f4c95c4fc9277aa1b2171c3ef73022fac
andermatt <matt10.anderson@gmail.com>
2019-04-29 14:46:37 -0400

    Adding static method to generate from an Award. Removed hard-coded values from default constructor.

--- src/main/java/github/paz/awardportal/pdf/AwardPdfTemplateData.java
+++ src/main/java/github/paz/awardportal/pdf/AwardPdfTemplateData.java
@@ -1,11 +1,17 @@
 package github.paz.awardportal.pdf;
 
+import github.paz.awardportal.model.Award;
+import github.paz.awardportal.model.User;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.text.SimpleDateFormat;
 
 @Getter
 @Setter
 public class AwardPdfTemplateData {
+    private static final String DATE_FORMAT = "dd MMM yyyy";
+    private static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat(DATE_FORMAT);
     private String awardName;
     private String recipientName;
     private String granterName;
@@ -13,14 +19,31 @@ public class AwardPdfTemplateData {
     private String signatureImage;  // Base64-encoded signature image.
     private Boolean hasSignature;
 
-    //public static AwardPdfTemplateData fromAward(Award award);
+    public static AwardPdfTemplateData fromAward(Award award) {
+        AwardPdfTemplateData data = new AwardPdfTemplateData();
+        // TODO - nullptr protection.
+        User recipient = award.getRecipient();
+        User granter = award.getGranter();
+
+        data.setAwardName(award.getAwardType().getName());
+        data.setRecipientName(recipient.getFirstName() + recipient.getLastName());
+        data.setGranterName(granter.getFirstName() + granter.getLastName());
+        data.setDateAwarded(DATE_FORMATTER.format(award.getTimestamp()));
+
+        // TODO - template currently uses a hard-coded image.
+        // This method will convert the granter's signature to a Base64-encoded
+        // string for the template.
+//        data.setSignatureImage(formatSignatureImage());
+        return data;
+
+    }
     // TODO - generate from an Award.
     public AwardPdfTemplateData() {
-        awardName = "Employee of the Month";
-        recipientName = "John Doe";
-        granterName = "Jane Doe";
-        dateAwarded = "4/22/2019";
-//        signatureImage = getSignatureImage();  Converts granter's Signature blob to Base64 string and sets bool.
-        hasSignature = false;
+//        awardName = "Employee of the Month";
+//        recipientName = "John Doe";
+//        granterName = "Jane Doe";
+//        dateAwarded = "4/22/2019";
+////        signatureImage = getSignatureImage();  Converts granter's Signature blob to Base64 string and sets bool.
+//        hasSignature = false;
     }
 }
