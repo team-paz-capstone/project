36d1b57af1ab5010afcf6ee9df351d37a9550aec
ziwei wu <ziweiwu@gmail.com>
2019-05-15 17:14:59 -0400

    added a loading bar to show during loading data

---
+++ src/main/js/components/LoadingBar.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React, {Component} from 'react';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const LoadingBar = ()=> {
+  return (
+    <div>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <LinearProgress/>
+    </div>
+  );
+};
+
+export default LoadingBar;
