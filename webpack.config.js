e1ede0a77d041d715fd39453823f0e9a0ab1c517
ziwei wu <ziweiwu@gmail.com>
2019-05-16 12:21:48 -0400

    reformat using eslint/prettier

--- webpack.config.js
+++ webpack.config.js
@@ -7,20 +7,22 @@ module.exports = {
   mode: 'development',
   output: {
     path: __dirname,
-    filename: './src/main/resources/static/built/bundle.js',
+    filename: './src/main/resources/static/built/bundle.js'
   },
   module: {
     rules: [
       {
         test: path.join(__dirname, '.'),
         exclude: /(node_modules)/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
-        }],
-      },
-    ],
-  },
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
