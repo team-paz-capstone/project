02fffc8d3af0ba5bde7848e64dea78766c57f025
ziwei wu <ziweiwu@gmail.com>
2019-05-22 20:40:49 -0400

    move theming to main app.js to keep primary and secondary color consistent across the app

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -4,27 +4,41 @@
 
 import 'babel-polyfill';
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 
-import AppBarImplemented from "./components/AppBarImplemented"
-import AdminPortal from "./components/AdminPortal";
-import UserPortal from "./components/UserPortal";
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import red from '@material-ui/core/colors/red';
+import AppBarImplemented from './components/AppBarImplemented';
+import AdminPortal from './components/AdminPortal';
+import UserPortal from './components/UserPortal';
+
+// setup the color for primary and secondary using theming
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: red
+  },
+  typography: { useNextVariants: true }
+});
 
 class App extends React.Component {
-
   render() {
-    console.debug("Rendering App!");
+    console.debug('Rendering App!');
     return (
       <div>
-        <AppBarImplemented/>
-        <BrowserRouter>
-          <Route path='/admin' component={AdminPortal}/>
-          <Route exact path='/' component={UserPortal}/>
-        </BrowserRouter>
+        {/* allow customize theme color */}
+        <MuiThemeProvider theme={theme}>
+          <AppBarImplemented />
+          <BrowserRouter>
+            <Route path="/admin" component={AdminPortal} />
+            <Route exact path="/" component={UserPortal} />
+          </BrowserRouter>
+        </MuiThemeProvider>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('react'));
+ReactDOM.render(<App />, document.getElementById('react'));
