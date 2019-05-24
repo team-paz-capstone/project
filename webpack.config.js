c76fc836ef9a570e7612e52fd5d997f7a7cc9ffe
ziwei wu <ziweiwu@gmail.com>
2019-05-24 16:39:43 -0400

    added watch Options

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,17 @@ const path = require('path');
 module.exports = {
   entry: './src/main/js/app.js',
   devtool: 'sourcemaps',
-  cache: true,
   mode: 'development',
   output: {
     path: __dirname,
     filename: './src/main/resources/static/built/bundle.js'
   },
+
+  watchOptions: {
+    ignored: /node_modules/,
+    poll: true
+  },
+
   module: {
     rules: [
       {
