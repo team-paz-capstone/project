5ebd81aa8c2a0d7a3a40bff76f6a770ddef52ccd
pattrickrice <patrickrice20@gmail.com>
2019-05-25 15:47:26 -0700

    remove unnessary debug statement

--- src/main/js/reducers/authentication.js
+++ src/main/js/reducers/authentication.js
@@ -6,7 +6,6 @@ const initialState = {
 };
 
 export default function (state = initialState, action) {
-    console.debug(action);
     switch (action.type) {
         case LOG_IN:
             return {
