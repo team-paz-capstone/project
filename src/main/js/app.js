b866f749172d51609814789c6abdfb592302c576
ziwei wu <ziweiwu@gmail.com>
2019-05-26 10:33:12 -0400

    change App entry point to index.js

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,24 +1,20 @@
 import 'babel-polyfill';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import blue from '@material-ui/core/colors/blue';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import red from '@material-ui/core/colors/red';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import Main from './components/Main';
-import {createStore, applyMiddleware} from 'redux'
-import {Provider} from 'react-redux'
-import {createLogger} from 'redux-logger'
-import thunk from 'redux-thunk'
-import reducer from './reducers'
+import reducer from './reducers';
 
 const middleware = [thunk];
 middleware.push(createLogger());
 
-const store = createStore(
-    reducer,
-    applyMiddleware(...middleware)
-);
+const store = createStore(reducer, applyMiddleware(...middleware));
 
 // setup the color for primary and secondary using theming
 const theme = createMuiTheme({
@@ -26,24 +22,24 @@ const theme = createMuiTheme({
     primary: blue,
     secondary: red
   },
-  typography: {useNextVariants: true}
+  typography: { useNextVariants: true }
 });
 
 class App extends React.Component {
   render() {
     return (
-        <Provider store={store}>
-          {/* Hooks the Redux store to our application and passes to child components*/}
-          <div>
-            {/* allow customize theme color */}
-            <CssBaseline/>
-            <MuiThemeProvider theme={theme}>
-              <Main></Main>
-            </MuiThemeProvider>
-          </div>
-        </Provider>
+      <Provider store={store}>
+        {/* Hooks the Redux store to our application and passes to child components */}
+        <div>
+          {/* allow customize theme color */}
+          <CssBaseline />
+          <MuiThemeProvider theme={theme}>
+            <Main />
+          </MuiThemeProvider>
+        </div>
+      </Provider>
     );
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('react'));
+export default App;
