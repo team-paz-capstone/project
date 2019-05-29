50935359b174c3381ab92d8798de23483ecca5c3
andermatt <matt10.anderson@gmail.com>
2019-05-29 19:35:56 -0400

    Add utility for retrieving current user.

---
+++ src/main/java/github/paz/awardportal/util/SecurityUtil.java
@@ -0,0 +1,22 @@
+package github.paz.awardportal.util;
+
+import github.paz.awardportal.model.User.User;
+import github.paz.awardportal.repository.UserRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
+public class SecurityUtil {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User getCurrentUser() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return userRepository.findByEmail((String) auth.getPrincipal());
+    }
+}
