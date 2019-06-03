e9ef9d69aef539a52f3d6f1d42e57b02d9533237
ziwei wu <ziweiwu@gmail.com>
2019-06-03 12:47:36 -0400

    added support Redux DevTools, install the "Redux DevTools" extension in chrome, and access redux tab from inspection.

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,37 +1,36 @@
 import 'babel-polyfill';
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import {MuiThemeProvider} from '@material-ui/core/styles';
-import {createStore, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
-import {createLogger} from 'redux-logger';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import MainLayout from './components/MainLayout';
 import reducer from './reducers';
-import {theme} from "./ui/theme"
+import { theme } from './ui/theme';
 
 let store;
 const middleware = [thunk];
 if (process.env.NODE_ENV === 'development') {
-  console.warn(
-      "Using Redux logger in development. Expect lower performance");
+  console.warn('Using Redux logger in development. Expect lower performance');
   middleware.push(createLogger());
 }
-store = createStore(reducer, applyMiddleware(...middleware));
-
+store = createStore(reducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 class App extends React.Component {
   render() {
     return (
-        <Provider store={store}>
-          <div>
-            {/* allow customize theme color */}
-            <MuiThemeProvider theme={theme}>
-              <CssBaseline/>
-              <MainLayout/>
-            </MuiThemeProvider>
-          </div>
-        </Provider>
+      <Provider store={store}>
+        <div>
+          {/* allow customize theme color */}
+          <MuiThemeProvider theme={theme}>
+            <CssBaseline />
+            <MainLayout />
+          </MuiThemeProvider>
+        </div>
+      </Provider>
     );
   }
 }
